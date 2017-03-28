3f70d2851673c1f89df40442980d6b46ddb7cc9c
Rémy Pottier <remy.pottier@mines-nantes.fr>
2017-03-28 09:49:51 +0200

    fix retrieving monitoring data

--- pidiou/pidiou-deploy.sh
+++ pidiou/pidiou-deploy.sh
@@ -5,7 +5,7 @@ CLUSTER="graphene"
 DURATION=6
 ENVIRONMENT="jessie-x64-min"
 
-echo "Reserving resources on cluster '$CLUSTER' for $DURATION hours"
+echo "$(date) - Reserving resources on cluster '$CLUSTER' for $DURATION hours"
 
 if [ -z "$CLUSTER" ]; then
   ../reserve.py -t $DURATION -n 1
@@ -17,26 +17,26 @@ fi
 end_seconds=$(( $(date -d "+$DURATION hours" +%s) - 600 ))
 end_date=$(date -d "+$DURATION hours")
 
-echo "The experiment will end at $end_date"
-echo "Deploying the environement jessie-min"
+echo "$(date) - The experiment will end at $end_date"
+echo "$(date) - Deploying the environement jessie-min"
 ../deploy.sh -e $ENVIRONMENT
 
 NODE=$(cat /tmp/tools/node2deploy.txt | awk '{ print $1 }')
-echo "Using the node $NODE to deploy pidiou"
+echo "$(date) - Using the node $NODE to deploy pidiou"
 scp metrics-pdu.py pdu-generator.py install.sh root@$NODE:
 ssh root@$NODE "./install.sh $SITE &> install.log &"
 now_seconds=$(date +%s)
 while [ $now_seconds -lt $end_seconds ]; do
-  echo "Waiting the end of the experiment at $end_date"
+  echo "$(date) - Waiting the end of the experiment at $end_date"
   sleep_time=3600
   now_seconds=$(date +%s)
   if [ $(( $end_seconds - $now_seconds )) -lt 3600 ]; then
     sleep_time=$(( $end_seconds - $now_seconds ))
   fi
-  echo "Sleeping $sleep_time seconds"
+  echo "$(date) - Sleeping $sleep_time seconds"
   sleep $sleep_time
 done
 
-echo "Retrieving the monitoring data"
-./monitoring-data.sh
+echo "$(date) - Retrieving the monitoring data"
+./monitoring-data.sh $NODE
 
